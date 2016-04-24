de7669f725bae8cce08876e14fec6ad897c89726
Ugnius <ugnius.malukas@gmail.com>
2016-04-24 22:08:47 +0300

    Added notifications send scheduler.

---
+++ laravel/app/Console/Commands/NotificationsSend.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Http\Controllers\NotificationController;
+use Illuminate\Console\Command;
+
+class NotificationsSend extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'notifications:send';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Send notifications about reached stock price.';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $controller = new NotificationController();
+        $controller->sendEmails();
+    }
+}
--- laravel/app/Console/Kernel.php
+++ laravel/app/Console/Kernel.php
@@ -13,18 +13,17 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        // Commands\Inspire::class,
+        Commands\NotificationsSend::class,
     ];
 
     /**
      * Define the application's command schedule.
      *
-     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
+     * @param  \Illuminate\Console\Scheduling\Schedule $schedule
      * @return void
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')
-        //          ->hourly();
+        $schedule->command('notifications:send')->everyMinute();
     }
 }
